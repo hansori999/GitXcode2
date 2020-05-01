cf34d158027756d6a65ad2e6eb304b4295b2eb1c
Myoung-Wan Koo <mwkoo9@gmail.com>
2020-05-01 19:34:06 +0900

    Initial Commit

---
+++ GitXcode2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode2
+//
+//  Created by Mac2019 on 2020/05/01.
+//  Copyright © 2020 Myoung-Wan Koo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
